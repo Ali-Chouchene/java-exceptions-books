b4045d2557ddc2499871d6c36f699ac076b8b71e
Ali-Chouchene <choucheneali9801@gmail.com>
2023-05-10 12:57:57 +0200

    init commit

--- src/org/lessons/java/book/Libro.java
+++ src/org/lessons/java/book/Libro.java
@@ -6,4 +6,44 @@ public class Libro {
 	private String autore;
 	private String editore;
 
+	public Libro(String titolo, int numPag, String autore, String editore) {
+
+		setTitolo(titolo);
+		setNumPag(numPag);
+		setAutore(autore);
+		setEditore(editore);
+	}
+
+	public String getTitolo() {
+		return titolo;
+	}
+
+	public void setTitolo(String titolo) {
+		this.titolo = titolo;
+	}
+
+	public int getNumPag() {
+		return numPag;
+	}
+
+	public void setNumPag(int numPag) {
+		this.numPag = numPag;
+	}
+
+	public String getAutore() {
+		return autore;
+	}
+
+	public void setAutore(String autore) {
+		this.autore = autore;
+	}
+
+	public String getEditore() {
+		return editore;
+	}
+
+	public void setEditore(String editore) {
+		this.editore = editore;
+	}
+
 }
